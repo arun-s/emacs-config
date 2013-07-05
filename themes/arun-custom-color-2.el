8447d80c1516a896738b130e233d5405317870b4
Arun Sivakumaran <aruns@outlook.com>
2013-07-05 14:03:25 -0400

    Adding color themes

---
+++ themes/arun-custom-color-2.el
@@ -0,0 +1,126 @@
+(defun arun-color-theme-2 ()
+  "Color theme by Arun Sivakumaran, based on Wombat for VIM."
+  (interactive)
+  (color-theme-install
+   '(my-color-theme
+      ((background-color . "black")
+      (background-mode . dark)
+      (border-color . "black")
+      (cursor-color . "LightGoldenrod")
+      (foreground-color . "limegreen")
+      (mouse-color . "yellow"))
+     (default
+       ((t (:stipple nil :background "#090909" :foreground "red"
+                     :inverse-video nil :box nil :strike-through nil
+                     :overline nil :underline nil :slant normal
+                     :weight normal :height 98 :width normal
+                     :family "outline-courier new"))))
+     (border ((t (:background "black"))))
+     (comint-highlight-input ((t (:bold t :weight bold))))
+     (comint-highlight-prompt ((t (:foreground "cyan"))))
+     (cursor ((t (:background "#656565"))))
+     (custom-button-face
+      ((t (:background "lightgrey" :foreground "black"
+                       :box (:line-width 2 :style released-button)))))
+     (custom-button-pressed-face
+      ((t (:background "lightgrey" :foreground "black"
+                       :box (:line-width 2 :style pressed-button)))))
+     (custom-changed-face ((t (:background "blue" :foreground "white"))))
+     (custom-comment-face ((t (:background "dim gray"))))
+     (custom-comment-tag-face ((t (:foreground "gray80"))))
+     (custom-documentation-face ((t (nil))))
+     (custom-face-tag-face
+      ((t (:bold t :weight bold :height 1.2 :family "helv"))))
+     (custom-group-tag-face
+      ((t (:bold t :foreground "light blue"
+                 :weight bold :height 1.2))))
+     (custom-group-tag-face-1
+      ((t (:bold t :foreground "pink" :weight bold
+                 :height 1.2 :family "helv"))))
+     (custom-invalid-face ((t (:background "red" :foreground "yellow"))))
+     (custom-modified-face ((t (:background "blue" :foreground "white"))))
+     (custom-rogue-face ((t (:background "black" :foreground "pink"))))
+     (custom-saved-face ((t (:underline t))))
+     (custom-set-face ((t (:background "gray15" :foreground "limegreen"))))
+     (custom-state-face ((t (:foreground "lime green"))))
+     (custom-variable-button-face ((t (:bold t :underline t :weight bold))))
+     (custom-variable-tag-face
+      ((t (:bold t :foreground "light blue"
+                 :weight bold :height 1.2 :family "helv"))))
+     (erc-action-face
+      ((t (:bold t :background "gray15"
+                 :foreground "lightyellow" :weight bold))))
+     (erc-bold-face
+      ((t (:bold t :background "gray15" :foreground "gray77" :weight bold))))
+     (erc-current-nick-face
+      ((t (:bold t :background "gray15" :foreground "lightblue" :weight bold))))
+     (erc-dangerous-host-face ((t (:background "gray15" :foreground "red"))))
+     (erc-default-face ((t (:background "Gray15" :foreground "gray77"))))
+     (erc-direct-msg-face ((t (:background "gray15" :foreground "gray90"))))
+     (erc-error-face ((t (:background "gray15" :foreground "red"))))
+     (erc-fool-face ((t (:background "gray15" :foreground "gray40"))))
+     (erc-input-face ((t (:background "gray15" :foreground "gray77"))))
+     (erc-inverse-face ((t (:background "gray77" :foreground "Gray15"))))
+     (erc-keyword-face
+      ((t (:bold t :background "gray15"
+                 :foreground "lightgreen" :weight bold))))
+     (erc-nick-default-face
+      ((t (:bold t :background "gray15" :foreground "Magenta" :weight bold))))
+     (erc-nick-msg-face
+      ((t (:bold t :background "gray15" :foreground "orange" :weight bold))))
+     (erc-notice-face
+      ((t (:bold t :background "gray15" :foreground "lightBlue" :weight bold))))
+     (erc-pal-face
+      ((t (:bold t :background "gray15" :foreground "white" :weight bold))))
+     (erc-prompt-face
+      ((t (:bold t :background "lightBlue2" :foreground "Black" :weight bold))))
+     (erc-timestamp-face
+      ((t (:bold t :background "gray15" :foreground "green" :weight bold))))
+     (erc-underline-face
+      ((t (:background "gray15" :foreground "gray77" :underline t))))
+     (extra-whitespace-face ((t (:background "pale green"))))
+     (fixed-pitch ((t (:family "courier"))))
+     (font-lock-builtin-face ((t (:foreground "LightSlateBlue"))))
+     (font-lock-comment-face ((t (:foreground "#99968b"))))
+     (font-lock-constant-face ((t (:foreground "#e5786d"))))
+     (font-lock-doc-face ((t (:foreground "#f6f3e8"))))
+     (font-lock-function-name-face ((t (:foreground "#e5786d"))))
+     (font-lock-keyword-face ((t (:foreground "#8ac6f2"))))
+     (font-lock-string-face ((t (:foreground "#95e454"))))
+     (font-lock-type-face ((t (:foreground "#cae682"))))
+     (font-lock-preprocessor-face((t (:foreground "#e5786d"))))
+     (font-lock-comment-delimiter-face ((t (:foreground "#f6f3e8"))))
+     (font-lock-variable-name-face ((t (:foreground "lightyellow"))))
+     (font-lock-warning-face ((t (:bold t :foreground "#e7f6da" :weight bold))))
+     (fringe ((t (:background "grey10"))))
+     (header-line ((t (:background "grey20" :foreground "grey90" :box nil))))
+     (highlight ((t (:background "darkolivegreen"))))
+     (jde-bug-breakpoint-cursor ((t (:background "brown" :foreground "cyan"))))
+     (jde-db-active-breakpoint-face ((t (:background "red" :foreground "black"))))
+     (jde-db-requested-breakpoint-face
+      ((t (:background "yellow" :foreground "black"))))
+     (jde-db-spec-breakpoint-face ((t (:background "green" :foreground "black"))))
+     (jde-java-font-lock-api-face ((t (:foreground "light goldenrod"))))
+     (jde-java-font-lock-bold-face ((t (:bold t :weight bold))))
+     (jde-java-font-lock-code-face ((t (nil))))
+     (jde-java-font-lock-constant-face ((t (:foreground "Aquamarine"))))
+     (jde-java-font-lock-doc-tag-face ((t (:foreground "light coral"))))
+     (jde-java-font-lock-italic-face ((t (:italic t :slant italic))))
+     (jde-java-font-lock-link-face
+      ((t (:foreground "blue" :underline t :slant normal))))
+     (jde-java-font-lock-modifier-face ((t (:foreground "LightSteelBlue"))))
+     (jde-java-font-lock-number-face ((t (:foreground "LightSalmon"))))
+     (jde-java-font-lock-operator-face ((t (:foreground "medium blue"))))
+     (jde-java-font-lock-package-face ((t (:foreground "steelblue1"))))
+     (jde-java-font-lock-pre-face ((t (nil))))
+     (jde-java-font-lock-underline-face ((t (:underline t))))
+     (menu ((t (nil))))
+     (mode-line
+      ((t (:background "grey75" :foreground "black"
+                       :box (:line-width -1 :style released-button)))))
+     (mouse ((t (:background "#656565"))))
+     (region ((t (:background "#cae682"))))
+     ;(scroll-bar ((t (nil))))
+     (secondary-selection ((t (:background "SkyBlue4"))))
+     (ecb-default-highlight-face ((t (:background "gray40" :height 1.0))))
+     )))
---
+++ themes/color-theme-subdued.el
@@ -0,0 +1,57 @@
+(defun color-theme-subdued ()
+  "Subdued color theme for Emacs by Jason Blevins.
+Based on the Tango color palette."
+  (interactive)
+  (color-theme-install
+   '(color-theme-subdued
+     ((foreground-color . "#628E06")
+      (background-color . "#202020")
+      (background-mode . dark)
+      (cursor-color . "#73d216") ; medium chameleon
+      (mouse-color . "#73d216"))
+
+     ;;; Standard font lock faces
+     (default ((t (nil))))
+     (font-lock-comment-face ((t (:italic t :foreground "#61635e")))) ; dark aluminum
+     (font-lock-comment-delimiter-face ((t (:foreground "#61635e")))) ; dark aluminum
+     (font-lock-doc-face ((t (:foreground "#77507b")))) ; plum
+     (font-lock-doc-string-face ((t (:foreground "#77507b")))) ; plum
+     (font-lock-string-face ((t (:foreground "#ADC877")))) ; plum
+     (font-lock-keyword-face ((t (:foreground "#729fcf")))) ; light sky blue
+     (font-lock-builtin-face ((t (:foreground "#855c1b")))) ; med-dark chocolate
+     (font-lock-function-name-face ((t (:foreground "#c4a000")))) ; dark butter
+     (font-lock-variable-name-face ((t (nil))))
+     (font-lock-preprocessor-face ((t (:foreground "#BD5960")))) ; aluminum
+     (font-lock-constant-face ((t (:foreground "#4e9a06")))) ; dark chameleon
+     (font-lock-type-face ((t (:foreground "#ad7fa8")))) ; light plum
+     (font-lock-warning-face ((t (:bold t :foreground "#cc0000")))) ; scarlet red
+
+     ;; Search
+     (isearch ((t (:foreground "#080808" :background "#edd400"))))
+     (isearch-lazy-highlight-face ((t (:foreground "#080808" :background "#2e3436"))))
+
+     ;; Emacs Interface
+     (fringe ((t (:background "#0f0f0f"))))
+     (border ((t (:background "#0f0f0f"))))
+     (mode-line ((t (:background "#AEADAD" :foreground "black"))))
+     (mode-line-buffer-id ((t (:background "#AEADAD" :foreground "black"))))
+     (mode-line-inactive ((t (:background "#AEADAD" :foreground "black"))))
+     (minibuffer-prompt ((t (:foreground "#729fcf")))) ; light sky blue
+     ;;(region ((t (:background "#2e3436"))))
+     (region ((t (:background "#6B4E26"))))
+     (scroll-bar ((t (:background "#6B4E26"))))
+
+     ;; Parenthesis matching
+     (show-paren-match-face ((t (:foreground "#2e3436" :background "#73d216"))))
+     (show-paren-mismatch-face ((t (:foreground "#2e3436" :background "#ef2929"))))
+
+     ;;highlight changes
+     (highlight-changes ((t (:foreground nil :background "#354555" :weight semi-bold))))
+     (highlight-changes-delete ((t (:foreground nil :background nil))))
+
+     ;; Info
+     (info-xref ((t (:foreground "#729fcf")))) ; light sky blue
+     (info-xref-visited ((t (:foreground "#ad7fa8")))) ; light plum
+
+     
+     )))
